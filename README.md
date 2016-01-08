b8c9579b1d0ac7376a02547d3d0ff2130e3fbad2
David Whitely <davydka@gmail.com>
2016-01-08 16:36:03 +0000

    Switching to mostly midi based setup.

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
 # hit
 
-Next steps:
-* Check voltage values on doepfer gate
-	* build common ground
-	* possible need safety resistor from doepfer hot value to arduino A0 input
-		* 27 - 47k ohm resistor in series with port pin
-* Javascript: 
-	* delta value
-	* threshold
-
-Possibly needed:
-* debounce
-	* https://lodash.com/docs#debounce
-	* https://davidwalsh.name/javascript-debounce-function
--- index.js
+++ index.js
@@ -1,60 +1,61 @@
 var _ = require('lodash');
-var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyACM0", {
-//var serialPort = new SerialPort("/dev/tty.usbmodem1421", {
-	  baudrate: 115200
-});
 var midi = require('midi');
 var output = new midi.output();
+var input = new midi.input();
+var input2 = new midi.input();
+
+for(var i=0; i < input2.getPortCount(); i++){
+	console.log(input2.getPortName(i));
+	//if( input2.getPortName(i) == 'mio 20:0' ){
+	//	input2.openPort(i);
+	//}
+	if( input2.getPortName(i) == 'LPK25 24:0' ){
+		input2.openPort(i);
+	}
+}
+
+for(var i=0; i < input.getPortCount(); i++){
+	console.log(input.getPortName(i));
+	if( input.getPortName(i) == 'mio 20:0' ){
+		input.openPort(i);
+	}
+	//if( input.getPortName(i) == 'LPK25 24:0' ){
+	//	input.openPort(i);
+	//}
+}
 
 for(var i=0; i < output.getPortCount(); i++){
 	if( output.getPortName(i) == 'mio 20:0' ){
 		output.openPort(i);
-		console.log('here');
 	}
 }
 
-serialPort.on("open", function () {
-	console.log('open');
-	
-	serialPort.write("ls\n", function(err, results) {
-		if(err){
-			console.log('err ' + err);
-		}
-		console.log('results ' + results);
-	});
-	
-
-	serialPort.on('data', function(data){ 
-		handleData(data);
-		//_.debounce( handleData(data), 35 );
-	});
+var noteOn = [144, 38, 100];
+var noteOff = [144, 38, 0];
+input2.on('message', function(deltaTime, message) {
+	console.log(message);
+	if(message[0]==144){
+		noteOn = [message[0], message[1], 127];
+		noteOff = [message[0], message[1], 0];
+	}
 });
 
-var oldData = 0;
-var handleData = function( newData ){
-	newData = parseInt(newData,16);
-	if(Number.isNaN(Number(newData)) ){
-		return;
-	}
-
-	var delta = newData - oldData;
-	//console.log( newData );
-	
-	if(delta > 0 && newData > 250){
-		console.log('hit ' + new Date());
+input.on('message', function(deltaTime, message) {
+	if(message[0]==144 && message[2]>0){
 		noteEvent();
+		//console.log(message);
 	}
+});
 
-	oldData = newData;
-}
 
 var noteEvent = function(){
-	output.sendMessage([144,38,100]);
+	output.sendMessage(noteOn);
+	//output.sendMessage([144,38,100]);
 	console.log('note on');
 
 	setTimeout(function(){
-		output.sendMessage([128,38,0]);
+		output.sendMessage(noteOff);
+		//output.sendMessage([128,38,0]);
 		console.log('note off');
 	}, 35)
 }
